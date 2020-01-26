592ee286c325d2acb6d56cb56c63ad6b3c6d8eaa
hossi <heidji@gmail.com>
2020-01-26 17:43:38 -0600

    safe area problem

--- src/app/hidenav/hidenav-stretchheader.component.ts
+++ src/app/hidenav/hidenav-stretchheader.component.ts
@@ -17,7 +17,6 @@ import {HidenavShService} from './hidenav-sh-service.service';
     template: `
         <style>
             .overlay {
-                padding-top:env(safe-area-inset-top);
                 position: absolute;
                 height: inherit;
                 width: inherit;
